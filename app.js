93622713fd5ddbf679bf7ffa9087e5fe06406f20
Josh L <joshua.levine42@gmail.com>
2024-10-31 23:15:25 -0400

    Functioning Scoreboard

--- app.js
+++ app.js
@@ -33,26 +33,27 @@ const pointValues = {
 let usedWords = [];
 // scoring variable
 let score = 0;
+
 //input field variable for js
 const inputElement = document.querySelector("input");
-console.dir(inputElement);
+
 // our button
-
 const buttonElement = document.querySelector("button");
-
 const scoreElement = document.querySelector("#score");
-console.dir(scoreElement);
 const scoringElement = document.querySelector("#scoring");
-console.dir(scoringElement);
+const highScoreElement = document.querySelector(".highScore");
+const bodyElement = document.querySelector("body");
+const gameContainerElement = document.querySelector(".gamecontainer");
 let userInput = "";
-
+const highScores = [];
 const clearInput = () => (inputElement.value = "");
 // resets the game
 const gameOver = () => {
-  score = 0;
   let totalWords = usedWords.length;
-  usedWords = [];
   scoringElement.textContent = `You repeated a word after entering ${totalWords} words. Game over! Enter any new word to play again!`;
+
+  score = 0;
+  usedWords = [];
   scoreElement.textContent = `${score} Points Earned`;
 
   clearInput();
@@ -109,15 +110,14 @@ const scoreWord = (word) => {
 
 const invalidInputAnimation = () => {
   inputElement.style.boxShadow = "10px 10px 10px red";
-//   the below will make the input box vibrate back and forth
-  const vibrate = setInterval(() =>{
+  //   the below will make the input box vibrate back and forth
+  const vibrate = setInterval(() => {
     // transform says we are moving it and translateX says where we are moving it on the X-axis.
-    inputElement.style.transform= 'translateX(-2px)';
-    setTimeout(()=>{
-        inputElement.style.transform = 'translateX(4px)';
-    },5)
-
-  },10)
+    inputElement.style.transform = "translateX(-2px)";
+    setTimeout(() => {
+      inputElement.style.transform = "translateX(4px)";
+    }, 5);
+  }, 10);
   // undoes the rest of the function after time in milliseconds elapses
   setTimeout(() => {
     inputElement.style.boxShadow = "";
@@ -125,8 +125,48 @@ const invalidInputAnimation = () => {
   }, 250);
 };
 
+const highScorefunction = () => {
+  let usersScore = 0;
+  usersScore = score;
+  const enterYourName = document.createElement("input");
+  let usersName = "";
+  //   disable the input and button until finished with score
+  inputElement.toggleAttribute("disabled");
+  inputElement.style.backgroundColor = "black";
+
+  enterYourName.style.width = "30%";
+  enterYourName.style.height = "15%";
+  enterYourName.placeholder = 'Type Your Name and Push "Enter" to Submit!';
+  enterYourName.style.padding = "10px";
+  enterYourName.style.zIndex = "1";
+  enterYourName.style.alignSelf = "center";
+  enterYourName.style.justifySelf = "center";
+
+  gameContainerElement.appendChild(enterYourName);
+  console.log(enterYourName);
+  enterYourName.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+      if (enterYourName.value !== "") {
+        usersName = enterYourName.value;
+        highScores.push({ userName: usersName, userScore: usersScore });
+        highScores.sort((low, high) => high.userScore - low.userScore);
+        highScoreElement.replaceChildren();
+
+        for (let i = 0; i < highScores.length; i++) {
+          let liElement = document.createElement("li");
+          liElement.textContent = `${highScores[i].userName}: ${highScores[i].userScore} Points`;
+
+          highScoreElement.appendChild(liElement);
+        }
+      }
+      gameContainerElement.removeChild(enterYourName);
+      inputElement.removeAttribute("disabled");
+      inputElement.style.backgroundColor = "";
+    }
+  });
+};
+
 const handleWordSubmission = () => {
-  console.time("myTimer");
   userInput = inputElement.value.toLowerCase().trim();
   if (userInput !== "") {
     if (notUsed() && validWord()) {
@@ -137,17 +177,17 @@ const handleWordSubmission = () => {
     } else if (notUsed() && validWord() === false) {
       invalidInputAnimation();
       scoreElement.textContent = `I'm sorry that word is not in our dictionary or it was misspelled. Please try again!`;
-    //   the below piece of code comes with help from chatgpt
-      if (/[^a-zA-Z]/.test(userInput)){
-        scoreElement.textContent = "I'm sorry you input an invalid character. Please make sure that you only input letters."
+      //   the below piece of code comes with help from chatgpt
+      if (/[^a-zA-Z]/.test(userInput)) {
+        scoreElement.textContent =
+          "I'm sorry you input an invalid character. Please make sure that you only input letters.";
       }
-
     } else {
+      highScorefunction();
       gameOver();
       clearInput();
     }
   }
-  console.timeEnd("myTimer");
 };
 
 // event listeners
