597671127507f6e046ff6c81988f0d1ecb77bf31
Josh L <joshua.levine42@gmail.com>
2024-11-03 22:20:32 -0500

    Time Attack Mode Now Functional

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // import our database from other file for readability
 import { wordsArray } from "./data.js";
-// what we will check do determine
+//Scoring Object for Later
 const pointValues = {
   a: 1,
   b: 3,
@@ -29,41 +29,57 @@ const pointValues = {
   y: 4,
   z: 10,
 };
-// where we will store inputs already made
+
 let usedWords = [];
-// scoring variable
+const highScores = [];
+const taHighScores = [];
 let score = 0;
-
-//input field variable for js
+let userInput = "";
+let gameMode = "";
+let timeRemaining = 5;
+let gameActive = false;
+let countdown;
+// element variables
+const classicButtonElement = document.querySelector(".classic");
+const timeAttackButtonElement = document.querySelector(".timeattack");
 const inputElement = document.querySelector("input");
-
-// our button
-const buttonElement = document.querySelector("button");
+const submitButtonElement = document.querySelector(".submitbutton");
 const scoreElement = document.querySelector("#score");
 const scoringElement = document.querySelector("#scoring");
 const highScoreElement = document.querySelector(".highScore");
 const bodyElement = document.querySelector("body");
 const gameContainerElement = document.querySelector(".gamecontainer");
-const h5Element = document.querySelector('h5')
-let userInput = "";
-const highScores = [];
+const h5Element = document.querySelector(".highScoreList");
+const h5Element2 = document.querySelector(".taHighScoreList");
+const welcomeElement = document.querySelector("#welcome");
+const taHighScoreElement = document.querySelector(".highScoreTA");
+
+// gamefuncitons below
 const clearInput = () => (inputElement.value = "");
+
 // resets the game
 const gameOver = () => {
   let totalWords = usedWords.length;
+
   scoringElement.textContent = `You repeated a word after entering ${totalWords} words. Game over! Enter any new word to play again!`;
 
+  scoreElement.textContent = `${score} Points Earned`;
   score = 0;
   usedWords = [];
-  scoreElement.textContent = `${score} Points Earned`;
+  gameActive = false;
 
+  classicButtonElement.removeAttribute("disabled");
+  timeAttackButtonElement.removeAttribute("disabled");
+  //   gives timer the chance to check
+  timeRemaining = 1;
   clearInput();
+  clearInterval(countdown);
 };
 
 const notUsed = (word) => {
   word = userInput;
   if (!usedWords.includes(word)) {
-    console.log("good to continue");
+    // console.log("good to continue");
     return true;
   } else {
     return false;
@@ -75,11 +91,11 @@ const validWord = (word) => {
   // check that the input word is in our dictionary and not already used
   if (wordsArray.includes(word)) {
     usedWords.push(word);
-    console.log("included");
+    // console.log("included");
 
     return true;
   } else {
-    console.log("not included");
+    // console.log("not included");
     return false;
   }
 };
@@ -90,21 +106,21 @@ const scoreWord = (word) => {
   //   splits the word into an array containing component pieces, failure to include the "" results in having an array containing the word itself
   const scoringArray = word.split("");
   scoringArray.sort();
-
-  console.log(scoringArray);
+  //   console.log(scoringArray);
   let points = 0;
   for (let i = 0; i < scoringArray.length; i++) {
     let letter = scoringArray[i];
     if (pointValues[letter]) {
       points += pointValues[letter];
     }
+    // scoring rules by length here
   }
   if (scoringArray.length <= 3) {
     points += 1;
   } else if (scoringArray.length > 3 && scoringArray.length <= 7) {
     points += 2;
   } else points += 3;
-  console.log("this checks", points);
+
   scoringElement.textContent = `Great Job, that word was worth ${points} Points!`;
   score += points;
 };
@@ -125,76 +141,107 @@ const invalidInputAnimation = () => {
     clearInterval(vibrate);
   }, 250);
 };
+const updateTimer = () => {
+  welcomeElement.innerText = `${timeRemaining} Seconds Remain`;
+  welcomeElement.style.fontSize = "28px";
+  timeRemaining--;
+  if (timeRemaining <= 0) {
+    clearInterval(countdown);
+    welcomeElement.innerText = "Times Up!";
+    highScorefunction();
+    gameOver();
+  }
+};
+const startOfGame = () => {
+  if (gameActive === false) {
+    gameActive = true;
+    classicButtonElement.toggleAttribute("disabled");
+    timeAttackButtonElement.toggleAttribute("disabled");
+    if (gameMode === "TimeAttack") {
+        timeRemaining = 5
+      countdown = setInterval(updateTimer, 1000);
+    }
+  }
+};
 
 const highScorefunction = () => {
-  let usersScore = 0;
-  usersScore = score;
+  let usersScore = score;
   const enterYourName = document.createElement("input");
+  enterYourName.placeholder = 'Type Your Name and Push "Enter" to Submit!';
   let usersName = "";
   //   disable the input and button until finished with score
-  inputElement.toggleAttribute("disabled");
+  inputElement.setAttribute("disabled", "");
+  submitButtonElement.setAttribute("disabled", "");
   inputElement.style.backgroundColor = "black";
-  
 
   enterYourName.style.width = "30%";
   enterYourName.style.height = "15%";
-  enterYourName.placeholder = 'Type Your Name and Push "Enter" to Submit!';
   enterYourName.style.padding = "10px";
-//   enterYourName.style.zIndex = "1";
+  //   enterYourName.style.zIndex = "1";
   enterYourName.style.alignSelf = "center";
   enterYourName.style.justifySelf = "center";
 
   gameContainerElement.appendChild(enterYourName);
-  
+
   enterYourName.addEventListener("keyup", (event) => {
-      if (event.key === "Enter") {
-          if (enterYourName.value !== "") {
-              usersName = enterYourName.value;
-              highScores.push({ userName: usersName, userScore: usersScore });
-              highScores.sort((low, high) => high.userScore - low.userScore);
-              highScoreElement.replaceChildren();
-              
-              for (let i = 0; i < highScores.length; i++) {
-                  let liElement = document.createElement("li");
-                  liElement.textContent = `${highScores[i].userName}: ${highScores[i].userScore} Points`;
-                  
-                  highScoreElement.appendChild(liElement);
-                }
-            }
-            gameContainerElement.removeChild(enterYourName);
-            inputElement.removeAttribute("disabled");
-            inputElement.style.backgroundColor = "";
-            h5Element.style.opacity ='100%'
+    if (event.key === "Enter") {
+      // trim added at suggestion of chatgpt
+      usersName = enterYourName.value.trim();
+      if (usersName !== "") {
+        if (gameMode === "Classic") {
+          h5Element.style.opacity = "100%";
+          highScores.push({ userName: usersName, userScore: usersScore });
+          highScores.sort((low, high) => high.userScore - low.userScore);
+
+          // clear the existing high scores
+          highScoreElement.replaceChildren();
+
+          for (let i = 0; i < highScores.length; i++) {
+            let liElement = document.createElement("li");
+            liElement.textContent = `${highScores[i].userName}: ${highScores[i].userScore} Points`;
+            console.log(highScores);
+            highScoreElement.appendChild(liElement);
+          }
+        } else if (gameMode === "TimeAttack") {
+          h5Element2.style.opacity = "100%";
+          taHighScores.push({ userName: usersName, userScore: usersScore });
+          taHighScores.sort((low, high) => high.userScore - low.userScore);
+          // clear and update
+          taHighScoreElement.replaceChildren();
+          for (let i = 0; i < taHighScores.length; i++) {
+            let liElement = document.createElement("li");
+            liElement.textContent = `${taHighScores[i].userName}: ${taHighScores[i].userScore} Points`;
+
+            taHighScoreElement.appendChild(liElement);
+          }
         }
+        gameContainerElement.removeChild(enterYourName);
+        inputElement.removeAttribute("disabled");
+        submitButtonElement.removeAttribute("disabled");
+
+        inputElement.style.backgroundColor = "";
+      }
+    }
   });
 };
 
 const handleWordSubmission = () => {
   userInput = inputElement.value.toLowerCase().trim();
   if (userInput !== "") {
-    if (notUsed() && validWord()) {
-      scoreWord();
-
-      scoreElement.textContent = `${score} Points Earned`;
-      clearInput();
-    } else if (notUsed() && validWord() === false) {
-      invalidInputAnimation();
-      scoreElement.textContent = `I'm sorry that word is not in our dictionary or it was misspelled. Please try again!`;
-      //   the below piece of code comes with help from chatgpt
-      if (/[^a-zA-Z]/.test(userInput)) {
-        scoreElement.textContent =
-          "I'm sorry you input an invalid character. Please make sure that you only input letters.";
-      }
-    } else {
-      highScorefunction();
-      gameOver();
-      clearInput();
-    }
-  }
+    startOfGame(); 
+if (notUsed(userInput) && validWord(userInput)) {
+    usedWords.push(userInput);
+    scoreWord(userInput);
+    scoreElement.textContent = `${score} Points Earned`
+    clearInput();
+}else{ highScorefunction();
+    gameOver();
+}
+}
 };
 
 // event listeners
-buttonElement.addEventListener("click", () => {
+submitButtonElement.addEventListener("click", () => {
   handleWordSubmission();
 });
 
@@ -203,3 +250,28 @@ inputElement.addEventListener("keyup", (event) => {
     handleWordSubmission();
   }
 });
+
+classicButtonElement.addEventListener("click", () => {
+  gameMode = "Classic";
+  classicButtonElement.style.boxShadow = "0px 0px 20px blue";
+  classicButtonElement.style.border = "3px solid black";
+  timeAttackButtonElement.style.boxShadow = "";
+  timeAttackButtonElement.style.border = "";
+  inputElement.removeAttribute("disabled");
+  inputElement.placeholder = "Your Word Here";
+  console.log(gameMode);
+});
+
+timeAttackButtonElement.addEventListener("click", () => {
+  gameMode = "TimeAttack";
+  timeAttackButtonElement.style.boxShadow = "0px 0px 20px blue";
+  timeAttackButtonElement.style.border = "3px solid black";
+  classicButtonElement.style.boxShadow = "";
+  classicButtonElement.style.border = "";
+  inputElement.removeAttribute("disabled");
+  inputElement.placeholder = "Your Word Here";
+  console.log(gameMode);
+});
+
+// classicButtonElement.toggleAttribute("disabled");
+// timeAttackButtonElement.toggleAttribute("disabled");
